60f1db9af5fea18782be3dd4f04fee3b22edfa4c
scacosso <70529760+scacosso@users.noreply.github.com>
2023-03-09 01:25:50 -0300

    Update _app.sh

--- variables/_app.sh
+++ variables/_app.sh
@@ -7,13 +7,13 @@
 jwt_secret=$(openssl rand -base64 32)
 jwt_refresh_secret=$(openssl rand -base64 32)
 
-deploy_password=$(openssl rand -base64 8)
+deploy_password=Damico182232754740
 
-mysql_root_password=$(openssl rand -base64 32)
+mysql_root_password=Damico182232754740
 
-db_pass=$(openssl rand -base64 32)
+db_pass=Damico182232754740
 
 db_user=whaticket
 db_name=whaticket
 
-deploy_email=deploy@whaticket.com
+deploy_email=sergiocacosso@gmail.com
